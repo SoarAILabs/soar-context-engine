366784dbc8dbf6e5484536b070c704fe5d95540c
Amaan Bilwar <bilwarad@mail.uc.edu>
2025-12-29 15:51:51 -0500

    updated queries and schema with test git indexer infor

--- db/schema.hx
+++ db/schema.hx
@@ -10,46 +10,26 @@ N::Branch{
     INDEX repo_id: String,
 	INDEX branch_id: String,
     name: String,
-    INDEX current_commit_id: String,
+    //INDEX current_commit_id: String,
     current_head: Boolean,
     has_remote: Boolean
 }
 
-
 // commit
 N::Commit{
-    INDEX branch_id: String,
-    INDEX commit_id: String,
+    INDEX parent_commit_id: String,
+    //INDEX branch_id: String,
+    INDEX commit_id: String, // needs to be sha
 	commit_message: String,
 	author_name: String,
 	author_email: String,
-	timestamp: Date DEFAULT NOW,
-	INDEX parent_commit_id: String,
-	changed_files_array: String,
-}
-
-// file Changes
-N::FileChanges{
-    INDEX commit_id: String,
-    INDEX file_id: String,
-    file_name: String,
-    file_path: String,
-    is_renamed: Boolean,
-    old_path: String DEFAULT "",
-    change_type: String,
-    //timestamp: Date DEFAULT NOW,
-    changed_diff_hunks_array: String
-}
-
-// diff hunks
-N::DiffHunks{
-    INDEX file_id: String,
-    INDEX diff_hunk_id: String,
-    old_start_position: I64,
-    old_start_count: I64,
-    new_line_position:I64,
-    new_line_count:I64,
-    diff: String,
+	no_of_files_changed: I32,
+	old_blob_sha: String,
+	new_blob_sha: String,
+	//timestamp: Date DEFAULT NOW,
+	diff_position: String,
+	diff_content:String,
+	//changed_files_array: String,
 }
 
 // edges
@@ -62,36 +42,14 @@ E::HasCommit{
     From: Branch,
     To: Commit
 }
-
-E::CommitHasDiffHunks{
+// commit to commit_vector
+E::HasCommitVector{
     From: Commit,
-    To: DiffHunks
+    To: CommitVector,
 }
 
-E::HasFileChanges{
-    From: Commit,
-    To: FileChanges
-}
-
-
-E::HasDiff{
-    From: Commit,
-    To: DiffHunks,
-}
-
-E::FileChangesHasDiffHunks{
-    From: FileChanges,
-    To: DiffHunks
-}
-
-E::DiffHunksToDiffHunksVector{
-    From: DiffHunks,
-    To: DiffHunksVector
-}
-
-// vector
-V::DiffHunksVector{
-    INDEX diff_hunk_id: String,
-    diff_content: String,
-    created_at: Date DEFAULT NOW
+V::CommitVector{
+    INDEX commit_id: String,
+	diff_position: String,
+	diff_content:String,
 }
